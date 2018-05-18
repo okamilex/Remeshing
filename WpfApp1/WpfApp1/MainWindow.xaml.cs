11a3e428a57906f491b72ce4a80ee56c8729da2a
DESKTOP-H3VU287\alex_ <alex41bu@gmail.com>
2018-05-18 08:00:30 +0300

    algo

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using HelixToolkit.Wpf;
 using System.Windows.Media.Converters;
 using System.Xml.Linq;
 using Model;
+using AdaptiveRemeshing;
 
 namespace WpfApp1
 {
@@ -38,7 +39,6 @@ namespace WpfApp1
         {
             get
             {
-                //return 0;
                 return Convert.ToDouble(Rand.Next(99)) / 100;
             }
         }
@@ -50,251 +50,45 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Remesh1();
+            Algo.Remesh1();
         }
 
-        public void Remesh1()
+        private void Button2_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 5; i++)
-            {
-                Split();
-                Colaps();
-                Flip();
-                Shift();
-            }
+             //Remesh2();
+             //Remesh2B();
+             Algo.Remesh2C();
         }
 
-        public void Remesh2()
+        private void Gen_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 5; i++)
-            {
-                Adapte();
-            }
+            Graph.Polygons = new List<Model.Polygon>();
+            nodes = new List<Node>();
+
+            genObj();
         }
 
-        public void Remesh2B()
+        private void Save_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 2; i++)
-            {
-                var polygons = Graph.Polygons.Where(p => p.Nodes.All(n => Math.Abs(n.D-p.Nodes[0].D)<0.5));
-                var listIDs = polygons.Select(p => p.PolygonID).ToList();
-                foreach (var polygon in listIDs)
-                {
-                    Graph.Polygons.RemoveAll(p => p.PolygonID == polygon);
-                }
-                AdapteB();
-            }
+            var sfd = new Microsoft.Win32.SaveFileDialog() { Filter = "3D objects |*.obj" };
+            var result = sfd.ShowDialog();
+            if (result == false) return;
+            SavePath = sfd.FileName;
+            SaveAndShow();
         }
 
-        public void Remesh2C()
+        private void Load_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 0; i < 10; i++)
-            {
-                Flip();
-                AdapteC();
-                //Shift2();
-                //SaveAndShow(i);
-            }
+            var ofd = new Microsoft.Win32.OpenFileDialog() { Filter = "3D objects |*.obj" };
+            var result = ofd.ShowDialog();
+            if (result == false) return;
+            LoadPath = ofd.FileName;
+
+            Graph.Polygons = new List<Model.Polygon>();
+            nodes = new List<Node>();
+
+            LoadObj();
         }
-
-        public void AdapteC()
-        {
-            var i = 0;
-            var m = Graph.Nodes.Max(n => n.NodeID);
-            while (true)
-            {
-                var edge = Graph.Nodes.OrderBy(n => n.NodeID).FirstOrDefault(e => e.NodeID > i);
-                if (edge == null || edge.NodeID > m)
-                    break;
-                i = edge.NodeID;
-                edge.AdaptNodeC();
-             }
-         }
-
-
-        public void AdapteB()
-        {
-            var i = 0;
-            var m = Edge.GetID((0));
-            while (true)
-            {
-                var edge = Graph.Edges.Where(e => e.D).FirstOrDefault(e => e.EdgeID > i);
-                if (edge == null || edge.EdgeID > m)
-                    break;
-                i = edge.EdgeID;
-                edge.AdaptEdgeB();
-
-            }
-
-        }
-
-        public void Adapte()
-        {
-            var list = Graph.Edges.OrderBy(e => e.Nodes.Max(n => n.R)).ToList().Select((Value, Index) => new { Value, Index });
-            foreach (var pare in list)
-            {
-                pare.Value.EdgeID = pare.Index + 1;
-            }
-            var i = 0;
-            var j = 0;
-            var k = 0;
-            var m = Edge.GetID((0));
-            while (true)
-            {
-                var min = Graph.Nodes.Min(n => n.X);
-                var max = Graph.Nodes.Max(n => n.X);
-                var edge = Graph.Edges.Where(e => e.Nodes.All(n => Math.Abs(n.X -min) < 0.5 || Math.Abs(n.X - max) < 0.5)).FirstOrDefault(e => e.EdgeID > i);
-                if (edge == null || edge.EdgeID > m)
-                    break;
-
-
-
-                i = edge.EdgeID;
-                edge.AdaptEdge();
-                 
-             }
-     
-        }
-
-        public void Split()
-        {
-            var i = 0;
-            var j = 0;
-            var k = 0;
-            var m = Edge.GetID((0));
-            while (true)
-            {
-                var edge = Graph.Edges.FirstOrDefault(e => e.EdgeID > i);
-                if (edge == null || edge.EdgeID > m)
-                    break;
-                i = edge.EdgeID;
-                
-                //if (i == 42 || test.Any(p => p < 2))
-                //if (i == 972 || Graph.Polygons.Any(p => p.Edges == null) ||
-                //    Graph.Polygons.Any(p => p.Edges.Any(e => e == null)) || Graph.Polygons.Any(p => p.Nodes.Count > 3))
-                //{
-                //    var ty = Graph.Polygons.Where(p => p.Nodes.Any(n => n.NodeID == 3));
-                //    var t = 0;
-                //    t = t + 5;
-                   
-                //}
-                //var test = Graph.Edges.Select(p =>
-                //    Graph.Polygons.Count(edgeP => edgeP.Nodes.Contains(p.A) && edgeP.Nodes.Contains(p.B)));
-                //if (test.Any(p => p < 2) || test.Any(p => p > 2))
-                //{
-                //    var tu =Graph.Edges.Where(p =>
-                //        Graph.Polygons.Count(edgeP => edgeP.Nodes.Contains(p.A) && edgeP.Nodes.Contains(p.B)) > 2).ToList();
-                //    var ty = Graph.Polygons.Where(p => p.Nodes.Any(n => n.NodeID == 3));
-                //    var t = 0;
-                //    t = t + 5;
-                //}
-                if (edge.Length > (4.0 / 3.0) * 1)
-                {
-                    j =  edge.EdgeID;
-                    edge.Split();
-                    //var ty = Graph.Polygons.Where(p => p.Nodes.Any(n => n.NodeID == 3));
-                }
-            }
-        }
-
-        public void Colaps()
-        {
-            var i = 0;
-            var j = 0;
-            var k = 0;
-            var m = Edge.GetID((0));
-            while (true)
-            {
-                var edge = Graph.Edges.FirstOrDefault(e => e.EdgeID > i);
-                if (edge == null || edge.EdgeID > m)
-                    break;
-                i = edge.EdgeID;
-
-                //if (i == 42 || test.Any(p => p < 2))
-                //if (i == 972 || Graph.Polygons.Any(p => p.Edges == null) ||
-                //    Graph.Polygons.Any(p => p.Edges.Any(e => e == null)) || Graph.Polygons.Any(p => p.Nodes.Count > 3))
-                //{
-                //    var ty = Graph.Polygons.Where(p => p.Nodes.Any(n => n.NodeID == 3));
-                //    var t = 0;
-                //    t = t + 5;
-
-                //}
-                //var test = Graph.Edges.Select(p =>
-                //    Graph.Polygons.Count(edgeP => edgeP.Nodes.Contains(p.A) && edgeP.Nodes.Contains(p.B)));
-                //if (test.Any(p => p < 2) || test.Any(p => p > 2))
-                //{
-                //    var tu =Graph.Edges.Where(p =>
-                //        Graph.Polygons.Count(edgeP => edgeP.Nodes.Contains(p.A) && edgeP.Nodes.Contains(p.B)) > 2).ToList();
-                //    var ty = Graph.Polygons.Where(p => p.Nodes.Any(n => n.NodeID == 3));
-                //    var t = 0;
-                //    t = t + 5;
-                //}
-                if (edge.Length < (4.0 / 5.0) / 1)
-                {
-                    k = edge.EdgeID;
-                    //var ty = Graph.Polygons.Where(p => p.Nodes.Any(n => n.NodeID == 3));
-                    edge.Colaps();
-                }
-            }
-        }
-
-
-        public void Flip()
-        {
-            var i = 0;
-            var j = 0;
-            var k = 0;
-            var m = Edge.GetID((0));
-            while (true)
-            {
-                var edge = Graph.Edges.FirstOrDefault(e => e.EdgeID > i);
-                if (edge == null || edge.EdgeID > m)
-                    break;
-                i = edge.EdgeID;
-
-                //if (i == 42 || test.Any(p => p < 2))
-                //if (i == 972 || Graph.Polygons.Any(p => p.Edges == null) ||
-                //    Graph.Polygons.Any(p => p.Edges.Any(e => e == null)) || Graph.Polygons.Any(p => p.Nodes.Count > 3))
-                //{
-                //    var ty = Graph.Polygons.Where(p => p.Nodes.Any(n => n.NodeID == 3));
-                //    var t = 0;
-                //    t = t + 5;
-
-                //}
-                //var test = Graph.Edges.Select(p =>
-                //    Graph.Polygons.Count(edgeP => edgeP.Nodes.Contains(p.A) && edgeP.Nodes.Contains(p.B)));
-                //if (test.Any(p => p < 2) || test.Any(p => p > 2))
-                //{
-                //    var tu =Graph.Edges.Where(p =>
-                //        Graph.Polygons.Count(edgeP => edgeP.Nodes.Contains(p.A) && edgeP.Nodes.Contains(p.B)) > 2).ToList();
-                //    var ty = Graph.Polygons.Where(p => p.Nodes.Any(n => n.NodeID == 3));
-                //    var t = 0;
-                //    t = t + 5;
-                //}
-                if (edge.Length < (4.0 / 5.0) / 1)
-                {
-                    k = edge.EdgeID;
-                    //var ty = Graph.Polygons.Where(p => p.Nodes.Any(n => n.NodeID == 3));
-                    edge.Flip();
-                }
-            }
-        }
-        public void Shift()
-        {
-            foreach (var node in Graph.Nodes)
-            {
-             node.Shift();   
-            }
-        }
-
-        public void Shift2()
-        {
-            foreach (var node in Graph.Nodes)
-            {
-                node.Shift2();
-            }
-        }
-
 
         void LoadObj()
         {
@@ -350,7 +144,7 @@ namespace WpfApp1
 
                 foreach (var node in Graph.Nodes.OrderBy(n => n.NodeID))
                 {
-                    swi = swi + "v " + (node.X - max  / 2) + " " + node.Y + " " + node.Z + "\n";
+                    swi = swi + "v " + (node.X - max / 2) + " " + node.Y + " " + node.Z + "\n";
                 }
                 foreach (var polygonForWrit in Graph.Polygons)
                 {
@@ -565,43 +359,6 @@ namespace WpfApp1
             Graph.Polygons.Add(polygon);
             polygon = new Model.Polygon(temp[1], tempN[1], tempN[0]);
             Graph.Polygons.Add(polygon);
-        }
-
-        private void Button2_Click(object sender, RoutedEventArgs e)
-        {
-             //Remesh2();
-             //Remesh2B();
-             Remesh2C();
-        }
-
-        private void Gen_Click(object sender, RoutedEventArgs e)
-        {
-            Graph.Polygons = new List<Model.Polygon>();
-            nodes = new List<Node>();
-
-            genObj();
-        }
-
-        private void Save_Click(object sender, RoutedEventArgs e)
-        {
-            var sfd = new Microsoft.Win32.SaveFileDialog() { Filter = "3D objects |*.obj" };
-            var result = sfd.ShowDialog();
-            if (result == false) return;
-            SavePath = sfd.FileName;
-            SaveAndShow();
-        }
-
-        private void Load_Click(object sender, RoutedEventArgs e)
-        {
-            var ofd = new Microsoft.Win32.OpenFileDialog() { Filter = "3D objects |*.obj" };
-            var result = ofd.ShowDialog();
-            if (result == false) return;
-            LoadPath = ofd.FileName;
-
-            Graph.Polygons = new List<Model.Polygon>();
-            nodes = new List<Node>();
-
-            LoadObj();
         }
     }
 }
